c347d246922cfc10c8212f9c26f6497671f69074
dev0Rot <ivanovskiy.davidik@gmail.com>
2026-01-22 22:21:10 +0200

    Add process check for x32dbg and x64dbg

--- CSGO-SIMPLE-INJECTOR/skDebug.h
+++ CSGO-SIMPLE-INJECTOR/skDebug.h
@@ -1,8 +1,10 @@
-﻿#pragma once
+#pragma once
 #include "skCrypter.h"
 #include <windows.h>
 #include <winternl.h>
 #include <intrin.h>
+#include <tlhelp32.h>
+#include <string>
 
 #pragma intrinsic(__readfsdword)
 #pragma intrinsic(__readgsqword)
@@ -23,15 +25,15 @@ namespace skd
     {
         using f_t = BOOL(WINAPI*)(HANDLE, PBOOL);
         auto f = (f_t)GetProcAddress(
-            GetModuleHandleA((char*)skCrypt("kernel32.dll")),
-            (char*)skCrypt("CheckRemoteDebuggerPresent")
+            GetModuleHandleA((const char*)skCrypt("kernel32.dll")),
+            (const char*)skCrypt("CheckRemoteDebuggerPresent")
         );
 
         BOOL r = FALSE;
         if (f)
             f(GetCurrentProcess(), &r);
 
-        return r;
+        return r != FALSE;
     }
 
     __forceinline bool q3()
@@ -45,8 +47,8 @@ namespace skd
             );
 
         auto n = (n_t)GetProcAddress(
-            GetModuleHandleA((char*)skCrypt("ntdll.dll")),
-            (char*)skCrypt("NtQueryInformationProcess")
+            GetModuleHandleA((const char*)skCrypt("ntdll.dll")),
+            (const char*)skCrypt("NtQueryInformationProcess")
         );
 
         if (!n)
@@ -59,7 +61,7 @@ namespace skd
             &v,
             sizeof(v),
             nullptr
-        ) == 0 && v;
+        ) == 0 && v != 0;
     }
 
     __forceinline bool q4()
@@ -77,13 +79,43 @@ namespace skd
     __forceinline bool q5()
     {
         SetLastError(0);
-        OutputDebugStringA((char*)skCrypt("sk"));
+        OutputDebugStringA((const char*)skCrypt("sk"));
         return GetLastError() != 0;
+    }
+
+    __forceinline bool q6()
+    {
+        HANDLE snap = CreateToolhelp32Snapshot(TH32CS_SNAPPROCESS, 0);
+        if (snap == INVALID_HANDLE_VALUE)
+            return false;
+
+        PROCESSENTRY32W pe{};
+        pe.dwSize = sizeof(pe);
+
+        if (Process32FirstW(snap, &pe))
+        {
+            do
+            {
+                char exeName[260] = { 0 };
+                size_t converted = 0;
+                wcstombs_s(&converted, exeName, pe.szExeFile, _TRUNCATE);
+
+                if (_stricmp(exeName, (const char*)skCrypt("x32dbg.exe")) == 0 || // simple process name check ( noob 0_0 ) 
+                    _stricmp(exeName, (const char*)skCrypt("x64dbg.exe")) == 0)
+                {
+                    CloseHandle(snap);
+                    return true;
+                }
+            } while (Process32NextW(snap, &pe));
+        }
+
+        CloseHandle(snap);
+        return false;
     }
 
     __forceinline bool chk()
     {
-        return q1() | q2() | q3() | q4() | q5();
+        return q1() || q2() || q3() || q4() || q5() || q6();
     }
 
     __forceinline void brk()
